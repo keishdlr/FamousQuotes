09ad1ef852fc8ba788500f324dd90d06c558e5ca
keisha De La Rosa <keishdlr@gmail.com>
2025-10-07 11:12:26 -0400

    keep going loop working

--- src/main/java/com/pluralsight/FamousQuotes.java
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -7,7 +7,6 @@ public class FamousQuotes {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        try {
             String[] quotes = {
                     "The only way to do great work is to love what you do.",
                     "Success is not final, failure is not fatal: It is the courage to continue that counts.",
@@ -20,33 +19,36 @@ public class FamousQuotes {
                     "You miss 100% of the shots you don’t take.",
                     "Start where you are. Use what you have. Do what you can."
             };
-            System.out.print("Pick a quote (select #1 - #10): ");
-            int index = scanner.nextInt();
-            index--;   // change number from range 1-10 to range 0-9 because index starts with 0
-            // As long as the user entered a number in the range
-            // this will work.  Otherwise, the index
-            // will be out of range.
-            System.out.println(quotes[index]);
-
             //Add a loop to the program asks the user if they want to see another
             //saying.  If they say yes, repeat the process.  DO NOT shut the application down
             //when if an exception occurs.  Just pick up with the next iteration.
 
-            System.out.println("Do you want to see another quote? Y / N");
             boolean keepGoing = true;
-            keepGoing = scanner.nextBoolean();
             while (keepGoing = true){
-                System.out.print("Pick a another quote (select #1 - #10): ");
-                int index = scanner.nextInt();
-                index--;   // change number from range 1-10 to range 0-9 because index starts with 0
+                try {
+                    System.out.print("Pick a quote (select #1 - #10): ");
+                    int index = scanner.nextInt();
+                    scanner.nextLine(); // consume leftover newline
+                    if (index >= 1 && index <= 10) {
+                        index--;
+                        System.out.println(quotes[index]);
+                    } else {
+                        System.out.println("Your number was out of range!");
+                   // change number from range 1-10 to range 0-9 because index starts with 0
                 // As long as the user entered a number in the range
                 // this will work.  Otherwise, the index
                 // will be out of range.
-                System.out.println(quotes[index]);
         }
-        catch (Exception e) {
-            System.out.println("Your number was out of range!");
-            e.printStackTrace();
-        }}
+        } catch (Exception e) {
+            System.out.println("Invalid input! Please enter a number between 1 and 10");
+            scanner.nextLine(); // clear the bad input
+        }
+
+        System.out.print("Do you want to see another quote? Y / N: ");
+        String response = scanner.nextLine().trim().toLowerCase();
+        keepGoing = response.equals("y");
+    }
+
         scanner.close();
-    }}
+        System.out.println("Thanks for browsing quotes!");
+}}
